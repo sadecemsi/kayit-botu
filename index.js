0114d20603fe476699ca0157d661b1c21b9bc8a9
MSİ <136392359+sadecemsi@users.noreply.github.com>
2024-09-19 20:25:25 +0300

    discord.gg/msidev

--- index.js
+++ index.js
@@ -31,21 +31,6 @@ for (const file of commandFiles) {
 }
 
 
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
-for (const file of eventFiles) {
-    const filePath = path.join(eventsPath, file);
-    const event = require(filePath);
-    if (event.once) {
-        client.once(event.name, (...args) => event.execute(...args));
-    } else {
-        client.on(event.name, (...args) => event.execute(...args));
-    }
-    console.log(`Event yüklendi: ${event.name}`);
-}
-
-
 client.once('ready', () => {
     console.log(`${client.user.tag} olarak giriş yapıldı!`);
     
